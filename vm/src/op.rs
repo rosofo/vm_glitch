037a499ea0b9d662cd4efcf705d37ae9555753ed
mini-ninja-64 <mini-ninja-64@users.noreply.github.com>
2024-12-18 23:20:46 +0000

    Add swap op

--- vm/src/op.rs
+++ vm/src/op.rs
@@ -1,5 +1,3 @@
-use super::Vm;
-
 #[derive(PartialEq, Eq, PartialOrd, Ord)]
 pub enum Opcode {
     /// Allow for extra space in the bytecode
@@ -12,6 +10,8 @@ pub enum Opcode {
     Jump,
     /// Copy sample `i` from the audio buffer into the bytecode. If there are multiple channels this will be the product.
     Sample,
+    /// Swap sample chunk `i` and `j` in the audio buffer and byte `i` for `j` in the bytecode. 
+    Swap,
 }
 
 pub enum Op {
@@ -19,4 +19,5 @@ pub enum Op {
     Flip(usize),
     Jump(usize),
     Sample(usize),
+    Swap(usize, usize)
 }
