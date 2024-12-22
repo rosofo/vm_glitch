3d0a6cab5267ff6cd90ce724e01e3e4be4f746ca
Robin Forbes <rsforbes0@gmail.com>
2024-12-22 04:55:43 +0000

    improve(audio): use dasp for increased buffer sizes

--- src/delay_buffer.rs
+++ src/delay_buffer.rs
@@ -1,46 +1,31 @@
+use dasp::ring_buffer::Fixed;
+use dasp::*;
+use nih_plug::buffer::Buffer;
+
 pub struct DelayBuffer {
-    pub buffer: Vec<Vec<f32>>,
+    pub buffer: Fixed<Vec<[f32; 2]>>,
 }
 
 impl DelayBuffer {
     pub fn new(len: usize) -> Self {
         Self {
-            buffer: vec![vec![0.0; len], vec![0.0; len]],
-        }
-    }
-    // TODO maybe make private and call in [ingest_audio]
-    /// Copy second half to first half in preparation for new samples
-    pub fn copy_to_back(&mut self) {
-        for chan in self.buffer.iter_mut() {
-            let len = chan.len();
-            chan.copy_within(len / 2..len, 0);
-        }
-    }
-    /// Copy audio buffer to second half of self
-    pub fn ingest_audio(&mut self, audio: &mut [&mut [f32]]) {
-        for (self_chan, audio_chan) in self.buffer.iter_mut().zip(audio.iter()) {
-            let self_len = self_chan.len();
-            for (self_sample, audio_sample) in self_chan
-                .iter_mut()
-                .skip(self_len / 2)
-                .zip(audio_chan.iter())
-            {
-                *self_sample = *audio_sample;
-            }
+            buffer: Fixed::from(vec![[0.0, 0.0]; len]),
         }
     }
 
-    /// Copy first half (oldest) to audio buffer
-    pub fn write_to_audio(&self, audio: &mut [&mut [f32]]) {
-        for (self_chan, audio_chan) in self.buffer.iter().zip(audio.iter_mut()) {
-            for (self_sample, audio_sample) in self_chan.iter().zip(audio_chan.iter_mut()) {
-                *audio_sample = *self_sample;
-            }
+    /// Push incoming samples to the back of the buffer
+    pub fn ingest_audio(&mut self, audio: &mut Buffer) {
+        let audio = audio.as_slice();
+        for (left, right) in audio[0].iter().zip(audio[1].iter()) {
+            self.buffer.push([*left, *right]);
         }
     }
 
-    /// Can't return `&mut [&mut [f32]]` like nih_plug does cause it requires unsafe nastiness
-    pub fn as_mut_slice(&mut self) -> &mut [Vec<f32>] {
-        &mut self.buffer
+    /// Copy front to the audio buffer
+    pub fn write_to_audio(&self, audio: &mut Buffer) {
+        for (frame, mut chan_iter) in self.buffer.iter().zip(audio.iter_samples()) {
+            *chan_iter.get_mut(0).unwrap() = frame[0];
+            *chan_iter.get_mut(1).unwrap() = frame[1];
+        }
     }
 }
