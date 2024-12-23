d93279d574ce75201b3c81b72753fa9d9e3c75b4
Robin Forbes <rsforbes0@gmail.com>
2024-12-23 23:50:02 +0000

    make sure endianness is consistent

--- vm/src/memory.rs
+++ vm/src/memory.rs
@@ -2,6 +2,7 @@ use std::ops::Index;
 
 use byte::*;
 use byte_slice_cast::*;
+use ctx::NATIVE;
 use dasp::*;
 
 /// A unified buffer with instructions and audio samples
@@ -35,7 +36,7 @@ pub struct Samples<'a> {
 impl Iterator for Samples<'_> {
     type Item = f32;
     fn next(&mut self) -> Option<Self::Item> {
-        self.bytes.read_with(&mut self.offset, LE).ok()
+        self.bytes.read_with(&mut self.offset, NATIVE).ok()
     }
 }
 
@@ -47,23 +48,26 @@ impl Memory {
             Area::Sample
         }
     }
-    fn get_as_sample(&self, index: usize) -> Option<f32> {
+    pub fn get_as_sample(&self, index: usize) -> Option<f32> {
         let mut offset = index;
-        self.buffer.read_with(&mut offset, LE).ok()
+        self.buffer.read_with(&mut offset, NATIVE).ok()
     }
 
-    fn iter_samples(&self) -> Samples {
+    /// Iterate over the 'sample' area of the buffer
+    pub fn iter_samples(&self) -> Samples {
         Samples {
             bytes: &self.buffer,
             offset: self.program_len,
         }
     }
 
-    fn slices(&self) -> (&[u8], &[f32]) {
+    /// Slice into the program and sample memory, with samples cast to f32
+    pub fn slices(&self) -> (&[u8], &[f32]) {
         let (bytes, sample_bytes) = self.buffer.split_at(self.program_len);
         (bytes, sample_bytes.as_slice_of().unwrap())
     }
-    fn slices_mut(&mut self) -> (&mut [u8], &mut [f32]) {
+    /// Mutably slice into the program and sample memory, with samples cast to f32
+    pub fn slices_mut(&mut self) -> (&mut [u8], &mut [f32]) {
         let (bytes, sample_bytes) = self.buffer.split_at_mut(self.program_len);
         (bytes, sample_bytes.as_mut_slice_of().unwrap())
     }
