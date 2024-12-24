6d343a6cce1374c717161bb408aed80a8f1bf149
Robin Forbes <rsforbes0@gmail.com>
2024-12-24 23:42:23 +0000

    feat: enhance unroll_repeat_group with group indexing logic

--- lang/src/compile.rs
+++ lang/src/compile.rs
@@ -1,3 +1,6 @@
+use std::iter::once;
+
+use itertools::Itertools;
 use vm::op::Op;
 
 use crate::{assemble::assemble, parse::Gtch};
@@ -17,6 +20,8 @@ pub fn compile(ast: &[Gtch]) -> Result<Vec<u8>, eyre::Report> {
         }
     }
 
+    println!("{:?}", ir);
+
     assemble(&ir, 512)
 }
 
@@ -27,22 +32,38 @@ fn unroll_repeat_group(repeats: usize, children: Vec<Gtch>) -> impl Iterator<Ite
     children
         .into_iter()
         .cycle()
-        .map(|mut node| {
+        .enumerate()
+        .map(move |(op_idx, mut node)| {
+            let group_idx = op_idx / len;
+
             node.copy_mut().map(|(i, j)| {
-                i.idx_mut().map(|i| *i += 1);
-                j.idx_mut().map(|j| *j += 1);
+                i.idx_mut().map(|i| *i += group_idx);
+                j.idx_mut().map(|j| *j += group_idx);
             });
             node.swap_mut().map(|(i, j)| {
-                i.idx_mut().map(|i| *i += 1);
-                j.idx_mut().map(|j| *j += 1);
+                i.idx_mut().map(|i| *i += group_idx);
+                j.idx_mut().map(|j| *j += group_idx);
             });
             node.jump_mut().map(|i| {
-                i.idx_mut().map(|i| *i += 1);
+                i.idx_mut().map(|i| *i += group_idx);
             });
             node.sample_mut().map(|i| {
-                i.idx_mut().map(|i| *i += 1);
+                i.idx_mut().map(|i| *i += group_idx);
             });
             node
         })
         .take(len * repeats)
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::parse;
+
+    use super::*;
+
+    #[test]
+    fn test_unrolling() {
+        let ast = parse::parse("[3 .0 1>2 ]").unwrap();
+        let bytecode = compile(&ast).unwrap();
+    }
 }
