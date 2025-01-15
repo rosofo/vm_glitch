cd51a8a53116425e4d496f5e88229d8ec4d12502
mini-ninja-64 <mini-ninja-64@users.noreply.github.com>
2025-01-15 20:12:51 +0000

    Move processor to a trait

--- processor/src/lib.rs
+++ processor/src/lib.rs
@@ -3,14 +3,15 @@ use bitfield_struct::bitfield;
 #[bitfield(u32)]
 pub struct Offsets {
     #[bits(16)]
-    bytecode_start: usize,
+    data_start: u16,
     #[bits(16)]
-    data_start: usize,
+    _padding: u16,
 }
 
 pub struct Registers {
     // Read-only registers
     offsets: Offsets,
+    memory_len: u32,
     pc: u32,
     // r1: u32,
     // r2: u32,
@@ -18,12 +19,27 @@ pub struct Registers {
     // r4: u32
 }
 
-pub struct Processor {
+impl Registers {
+    pub fn reset(&mut self) {
+        self.pc = 0;
+    }
+}
+
+pub struct VmProcessor {
     registers: Registers,
 }
 
-impl Processor {
-    pub fn process(mut buffer: &[u8]) {
-        todo!("weeeeeee :3")
+impl Processor for VmProcessor {
+    fn reset(&mut self) {
+        todo!()
     }
+
+    fn run(&mut self, memory: & mut [u8]) {
+        todo!()
+    }
+}
+
+pub trait Processor {
+    fn reset(&mut self);
+    fn run(&mut self, memory: & mut [u8]);
 }
